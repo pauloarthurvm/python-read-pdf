26b5ed77c3c0c75b5173a74f5bcad5a2893cad7c
Paulo Varassin <pauloarthurvm@hotmail.com>
2023-10-11 21:50:42 -0300

    Separating lines

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ pathClear001 = ".\\old-files\\NotaNegociacao_20190718.pdf"
 pathXp001 = ".\\old-files\\c1b2d275-60b2-4d66-9d53-7a069b4295c7.pdf"
 pathAvenue001 = ".\\old-files\\Document_1092023_65401_AM_DnYqtYMG.pdf"
 
-reader = PyPDF2.PdfReader(pathXp001)
+reader = PyPDF2.PdfReader(pathAvenue001)
 
 print(f"pages length = {len(reader.pages)}")
 
 for page in range(len(reader.pages)):
-    print(f"content={(reader.pages[page-1].extract_text())}")
+    pageContent = (reader.pages[page-1].extract_text().split("\n"))
+    for index, text in enumerate(pageContent):
+        print(f"{index} : {text}")
