a0da085db78866ec822667f7d2c90ebe12bbae7b
Paulo Varassin <pauloarthurvm@hotmail.com>
2023-10-11 22:03:17 -0300

    Iterate and read files

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ pathClear001 = ".\\old-files\\NotaNegociacao_20190718.pdf"
 pathXp001 = ".\\old-files\\c1b2d275-60b2-4d66-9d53-7a069b4295c7.pdf"
 pathAvenue001 = ".\\old-files\\Document_1092023_65401_AM_DnYqtYMG.pdf"
 
-reader = PyPDF2.PdfReader(pathAvenue001)
+# reader = PyPDF2.PdfReader(pathAvenue001)
 
-print(f"pages length = {len(reader.pages)}")
+# print(f"pages length = {len(reader.pages)}")
 
 # list to store files
 listOfFiles = []
 oldFilesDir = ".\\old-files"
 for file in os.listdir(oldFilesDir):
-    if os.path.isfile(os.path.join(oldFilesDir, file)):
+    file: str
+    if os.path.isfile(os.path.join(oldFilesDir, file)) and file.endswith(".pdf"):
         listOfFiles.append(oldFilesDir + "\\" + file)
-        
-for page in range(len(reader.pages)):
-    pageContent = (reader.pages[page-1].extract_text().split("\n"))
-    for index, text in enumerate(pageContent):
-        print(f"{index} : {text}")
+
+for file in listOfFiles:
+    reader = PyPDF2.PdfReader(file)
+    for page in range(len(reader.pages)):
+        pageContent = (reader.pages[page-1].extract_text().split("\n"))
+        for index, text in enumerate(pageContent):
+            print(f"{index} : {text}")
